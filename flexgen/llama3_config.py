e6e48956d5e3f19dd53e4b2c7aeef3ce3fee679d
Dongliang <2720609228@qq.com>
2025-03-28 14:52:45 +0800

    read file

--- flexgen/llama3_config.py
+++ flexgen/llama3_config.py
@@ -63,7 +63,7 @@ class LlamaConfig:
 
 def get_llama_config(name, **kwargs):
     if "/" in name:
-        name = name.split("/")[3]
+        name = name.split("/")[2]
 
     if "-chat" in name:
         arch_name = name.replace("-chat", "")
---
+++ test_flexgen_speed.py
@@ -0,0 +1,69 @@
+import os
+import json
+import subprocess
+from datetime import datetime
+
+# model = "llama-3.1-8b-instruct"
+model = "llama-3.2-3b-instruct"
+offload_disk = True
+input_folder = '/mnt/LongBench-v2/extracted_contexts/'
+output_folder = f'/mnt/FlexGen/test_output_cuda/{model}/'
+
+def run_llama_cli(file_path, prompt_len, output_path):
+    env = os.environ.copy()
+
+    cpu_percent = 100
+    if offload_disk:
+        cpu_percent = 0
+    
+    command = [
+        "python3", 
+        "-m", "flexgen.flex_llama3",
+        "--model", f"/mnt/{model}", 
+        "--path", "/mnt/llama_weights", 
+        "--offload-dir", "/mnt/FlexGen/offload_dir", 
+        "--file", file_path,
+        "--prompt-len", str(prompt_len),
+        "--gen-len", "10",
+        "--gpu-batch-size", "1",
+        "--percent", "100", "0", "0", str(prompt_len), "100", "0", 
+        "--attn-sparsity", "0.1",
+        "--log-file-dir", output_folder,
+    ]
+
+    if model == "llama-3.1-8b-instruct":
+        command += ["--compress-weight"]
+
+    result = subprocess.run(command, capture_output=True, text=True, env=env)
+    print(result)
+
+lengths = [\
+            1000, \
+            # 2000, \
+            # 4000, \
+            # 8000, \
+            # 16000, \
+            # 32000, \
+            # 64000, \
+            # 128000, \
+]
+
+for length in lengths:
+    length_folder = os.path.join(input_folder, f"context_{length}")
+    test_output_folder = os.path.join(output_folder, f"context_{length}")
+    if not os.path.exists(test_output_folder):
+        os.makedirs(test_output_folder)
+
+    test_case_num = 3
+
+    if length == 128000:
+        test_case_num = 2
+
+    count = 0
+    for filename in os.listdir(length_folder):
+        if filename.endswith('.txt'):
+            count += 1
+            file_path = os.path.join(length_folder, filename)
+            run_llama_cli(file_path, length, test_output_folder)
+            if count >= test_case_num:
+                break
