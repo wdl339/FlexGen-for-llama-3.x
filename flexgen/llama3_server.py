2f6ce551ef698d5ca633ba5f9de970575d6814a6
wdl339 <2720609228@qq.com>
2025-04-01 17:28:46 +0800

    support qwen in server

--- flexgen/llama3_server.py
+++ flexgen/llama3_server.py
@@ -1,12 +1,15 @@
 from transformers import AutoTokenizer
 from flexgen.compression import CompressionConfig
 from flexgen.llama3_config import get_llama_config
+from flexgen.qwen2_config import get_qwen_config
 from flexgen.pytorch_backend import TorchDisk, TorchMixedDevice
 from flexgen.llama_backend import Llama3TorchDevice, fix_recursive_import
+from flexgen.qwen_backend import QwenTorchDevice
 from flexgen.flex_opt import Policy
 from flexgen.timer import timers
 from flexgen.utils import ExecutionEnv, get_test_inputs
 from flexgen.flex_llama3 import LlamaLM
+from flexgen.flex_qwen2 import QwenLM
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List, Dict, Union
@@ -15,19 +18,22 @@ import threading
 
 fix_recursive_import()
 
-MODEL = "/disk2/wdl/llama-3.2-3b-instruct"
+# MODEL = "/disk2/wdl/llama-3.2-3b-instruct"
+MODEL = "/disk2/wdl/llama-3.1-8b-instruct"
 PATH = "/disk2/wdl/FlexGen/llama_weights"
+# MODEL = "/disk2/wdl/qwen-2.5-7b-instruct"
+# PATH = "/disk2/wdl/FlexGen/qwen_weights"
 OFFLOAD_DIR = "/disk2/wdl/FlexGen/offload_dir"
 CUT_GEN_LEN = None
 DEBUG_MODE = None
 GPU_BATCH_SIZE = 1
 NUM_GPU_BATCHES = 1
 PREFILL_BATCH_SIZE = 512
-PERCENT = [100, 0, 100, 0, 100, 0]
+PERCENT = [100, 0, 0, 100, 100, 0]
 SEP_LAYER = True
 PIN_WEIGHT = True
 CPU_CACHE_COMPUTE = False
-ATTN_SPARSITY = 0.2
+ATTN_SPARSITY = 0.1
 COMPRESS_WEIGHT = False
 COMPRESS_CACHE = False
 VERBOSE = 2
@@ -38,10 +44,15 @@ tokenizer.pad_token_id = tokenizer.eos_token_id
 num_prompts = NUM_GPU_BATCHES * GPU_BATCH_SIZE
 cut_gen_len = CUT_GEN_LEN
 
-llama_config = get_llama_config(MODEL, pad_token_id=tokenizer.eos_token_id)
+if "llama" in MODEL:
+    model_config = get_llama_config(MODEL, pad_token_id=tokenizer.eos_token_id)
+    gpu = Llama3TorchDevice("cuda:0", rope_config=model_config.rope_config)
+    cpu = Llama3TorchDevice("cpu", rope_config=model_config.rope_config)
+elif "qwen" in MODEL:
+    model_config = get_qwen_config(MODEL, pad_token_id=tokenizer.eos_token_id)
+    gpu = QwenTorchDevice("cuda:0", rope_config=model_config.rope_config)
+    cpu = QwenTorchDevice("cpu", rope_config=model_config.rope_config)
 
-gpu = Llama3TorchDevice("cuda:0", rope_config=llama_config.rope_config)
-cpu = Llama3TorchDevice("cpu", rope_config=llama_config.rope_config)
 disk = TorchDisk(OFFLOAD_DIR)
 env = ExecutionEnv(gpu=gpu, cpu=cpu, disk=disk, mixed=TorchMixedDevice([gpu, cpu, disk]),
                       clear_cache=False)
@@ -60,7 +71,10 @@ policy = Policy(GPU_BATCH_SIZE, NUM_GPU_BATCHES,
                                       group_dim=2, symmetric=False))
 
 print("init weight...")
-model = LlamaLM(llama_config, env, PATH, policy, PREFILL_BATCH_SIZE)
+if "llama" in MODEL:
+    model = LlamaLM(model_config, env, PATH, policy, PREFILL_BATCH_SIZE)
+elif "qwen" in MODEL:
+    model = QwenLM(model_config, env, PATH, policy, PREFILL_BATCH_SIZE)
 
 warmup_inputs = get_test_inputs(32, num_prompts, tokenizer)
 print("warmup...")
@@ -84,12 +98,15 @@ def run_flexgen(chat, max_tokens):
 
     result = tokenizer.batch_decode(output_ids, skip_special_tokens=False, clean_up_tokenization_spaces=False)[0]
     
-    if "Answer:" in result:
+    # for Longbench dataset
+    result = result.replace(message,"")
+    
+    while "Answer:" in result:
         result = result.split("Answer:", 1)[1]
     
-    return result.replace(message,"")\
-                    .replace("<|start_header_id|>assistant<|end_header_id|>\n\n","")\
+    return result.replace("<|start_header_id|>assistant<|end_header_id|>\n\n","")\
                     .replace("<|start_header_id|>assistant<|end_header_id|>","")\
+                    .replace("<|start_header_id|>\n\n","")\
                     .replace("<|start_header_id|>","")\
                     .replace("<|end_header_id|>","")\
                     .replace("<|eot_id|>","")
